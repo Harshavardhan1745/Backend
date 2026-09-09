2c8120cb45a6067c98f4754e2300c319ef13ec49
Harshavardhan <iamharsha1745email@gmail.com>
2026-09-09 19:10:52 +0530

    updated

---
+++ JS/Day1/assets/index.js
@@ -0,0 +1,34 @@
+// var code 
+// var node 
+// var card 
+// var box 
+// var btn 
+// var hub 
+// var js 
+// var react 
+// var html 
+// var run 
+// var script 
+// var jsx 
+// var zone
+// var sink 
+// var sets 
+// var com 
+// var able 
+// var con 
+// var call 
+// var datatype 
+// var dt 
+// var arrofobj 
+// var arr 
+// var obj 
+// var not 
+// var null 
+// var non 
+// var end 
+// var start 
+// var cent 
+
+// Java script is the programming language and by using js 
+// we can able to create interactive and dynamic website and also 
+// it adds functionality and behavior 
