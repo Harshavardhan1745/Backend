e1575821c2a47e4c400b4a9ce969bb97e5c7348d
Harshavardhan <iamharsha1745email@gmail.com>
2026-09-09 19:37:15 +0530

    updated

---
+++ JS/Day2/assets/for.js
@@ -0,0 +1,23 @@
+for (let a = 1 ; a <= 10 ; a++){
+    console.log(a); 
+}
+
+for (let b = 0 ; b <= 20 ; b++ ){
+    if (b % 2 === 0) {
+        console.log(b);
+        
+    }
+}
+
+let c = 5 
+
+for (let d = 1 ; d <= 10 ; d++){
+    console.log(`${c} x ${d} = ${c*d}`);
+    
+}
+
+let f = " "
+for (let e = 1 ; e <= 5 ; e++){
+    console.log(f += e);
+    
+}
