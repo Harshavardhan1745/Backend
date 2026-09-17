73eaf96e747259e4231e172ea2d39ae907a49a0b
Harshavardhan <iamharsha1745email@gmail.com>
2026-09-17 12:00:13 +0530

    updated

--- JS/Day4/assets/hoam.js
+++ JS/Day4/assets/hoam.js
@@ -1,13 +1,89 @@
-function  createemp(){
-    let salary =20000
-    const updatedsalary = (amount) =>{
-        salary = salary + amount    
+// function  createemp(){
+//     let salary =20000
+//     const updatedsalary = (amount) =>{
+//         salary = salary + amount    
         
+//     }
+
+//     return updatedsalary
+// }
+
+// const updatedsalary = createemp ()  
+
+// console.log(updatedsalary(5000));
+
+
+// function add(a,b ,result){
+//    result(a+b) 
+// }
+// function cal(ans){
+//     console.log(`final answer : ${ans}` );
+    
+// }
+// add(10,20,cal)
+
+// function greet(name,callback){
+      
+//   callback(name)
+// }
+// function addname(name){
+//     console.log(`Hello ${name}`);
+// }
+// greet("Harsha",addname)
+
+// for (let i = 1; i <= 3; i++) {
+
+//     for (let j = 1; j <= 4; j++) {
+//         console.log("*");
+//     }
+// }
+
+// function multi(a,b,result){
+//     result (a*b)
+// }
+// function mul(ans){
+//     console.log(`answer is ${ans}`);
+    
+// }
+// multi(2,2,mul)
+
+// function nextlevel(num , eo){
+//      eo(num)
+// }
+// function evo(sum){
+//     if(sum%2===0){
+//         console.log(`${sum} is odd`);
+//     }else{
+//         console.log(`${sum} is odd`);
+        
+//     }
+    
+// }
+// nextlevel(7,evo)
+
+// function array(arr, callback){
+//     for(let i=0; i<= arr.length ; i++){
+//         callback(arr[i]);
+        
+//     }
+// }
+// function arrayprocess(loop){
+//     if(loop%2===0){
+//          console.log(`even number ${loop}`);
+         
+//     }
+// }
+// array([1,2,3,4,5],arrayprocess)
+
+function calttl(num ,callback){
+    let summ = 0
+    for (let i=0 ; i<=num.length; i++){
+        summ = summ + num[i]
     }
-
-    return updatedsalary
+    callback(summ)
 }
-
-const updatedsalary = createemp ()  
-
-console.log(updatedsalary(5000));
+function total(sum){
+    console.log(`Total is ${sum}`);
+    
+}
+calttl([50,50],total)
---
+++ JS/Day5/assets/arr.js
@@ -0,0 +1,27 @@
+const arr  = ["harsha","pooja","sam","pradii","rithi"]
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[4]);
+console.log(arr);
+
+let array = [1500, 2500, 3500, 4500, 5500]
+
+let sum =  array[0] + array[4]
+
+console.log(sum);
+
+console.log(array.splice(0,3));
+
+let skill = ["HTML", "CSS", "JavaScript", "React", "Node"]
+
+console.log(skill[2]);
+console.log(skill[3]);
+console.log(skill[skill.length-1]);
+
+
+
+
+
+
+
