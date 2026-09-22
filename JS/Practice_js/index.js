e056c7cf35f121ccd31b5df3b27b26624c3d504d
Harshavardhan <iamharsha1745email@gmail.com>
2026-09-23 01:17:57 +0530

    updated

--- JS/Practice_js/index.js
+++ JS/Practice_js/index.js
@@ -1,18 +1,39 @@
-// // let i 
-// // let j 
-// // let row 
-//  let n = 11
 
-// for (let i = 0 ; i <n ; i++){
-//   let row = " "
-//   for (j = 0 ; j = n ; j++){
-//     if(i == 0 || i == n-1 || j == 0 || j == n-1 || j == Math.floor(n/2)){
+// let row; 
+// let n = 16; 
+// for (i = 0 ; i < n ; i++){
+//   row = " " ;
+//   for (j = 0 ; j < n ; j++){
+//     if(i==0 || i==n-1 || j==0 || j==n-1 || j==Math.floor(n/2) || i==Math.floor(n/2) || i==j || i+j==n-1 || i+j==Math.floor(n/2)|| i-j==Math.floor(n/2) || i+j == (n-1)+Math.floor(n/2) || j-i ==Math.floor(n/2)) 
+//     {
 //       row = row + "*"
-//     }else{
-//       row = row + " "
+//     } else {
+//       row = row + " " 
 //     }
-
 //   }
 //   console.log(row);
-  
 // }
+
+
+// let row
+
+// for(let i=1; i<=5;i++){
+//     row = " "
+//     for(let j=1 ;j<=5;j++){
+//     row = row + "*"
+//     }
+//     console.log(row);
+// }
+
+let arr = [1,2,3,4,5,6,7,8]
+
+for (let i= arr.length; i>=0 ; i--){
+    console.log(arr[i]);
+    
+}
+
+for(let x in arr){
+    console.log(x);
+    
+}
+
---
+++ REACT/Day1/intro/src/App.jsx
@@ -0,0 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+const App = () => {
+  return (
+    <>
+      <div className="p-5 bg-gradient-to-r from-orange-500 to-white text-black m-5 rounded-lg shadow-2xl">
+        <div className="flex justify-between">
+          <div>
+          <h1 className="text-xl font-black">1. What is React?</h1>
+          <p>React is a JavaScript library used to build user interfaces, especially component-based web applications. </p>
+          <h1 className="text-xl font-black">2. Why do we use React?</h1>
+          <p>React is a JS libary so they make dynamic web site without the page reload entier webpage</p>
+          <h1 className="text-xl font-black">3. Is React a library or framework?</h1>
+          <p>The framework  will decide everything what have to  do in framework there will be some set of rules </p>
+         </div>
+         <div className="text-4xl mx-10">
+          <FontAwesomeIcon icon={faHeart } className="text-pink-400 text-bold animate-pulse" />  
+         </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
