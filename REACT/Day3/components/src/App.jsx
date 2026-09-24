fe3430c3e1517c42111128ddb1c4fef0c9a8ade2
Harshavardhan <iamharsha1745email@gmail.com>
2026-09-24 19:46:26 +0530

    updated

---
+++ REACT/Day3/components/src/App.jsx
@@ -0,0 +1,18 @@
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./components/Home"
+
+
+const App = () => {
+  return (
+    <>
+    <Home/>
+    <Header/>
+    <About/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ REACT/Day3/components/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <>
+    <h1>This is my footer section</h1>
+    </>
+  )
+}
+
+export default Footer
